34355fbe5a60d4d841cdf3c3f8f6c03bca2d69f6
sahilsheikh21 <sheikhsahil0921@gmail.com>
2026-06-06 20:49:54 +0400

    chore(app): remove placeholder comment from HaloApplication (TODO-7)

--- app/src/main/java/com/halo/HaloApplication.kt
+++ app/src/main/java/com/halo/HaloApplication.kt
@@ -7,6 +7,5 @@ import dagger.hilt.android.HiltAndroidApp
 class HaloApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Matrix SDK initialization will go here
     }
 }
